9b18fb120dbad6b339e28ca8fb3dc1ee74b54313
TofayelTaj <tajuddinahmed450@gmail.com>
2022-08-02 12:24:40 -0700

    project created

---
+++ src/main/java/com/example/dailyNotesAPI/DailyNotesApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.dailyNotesAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyNotesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyNotesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dailyNotesAPI/DailyNotesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dailyNotesAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyNotesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
